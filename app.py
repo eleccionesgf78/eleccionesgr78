0740ced549fe689011f214d626f1b76568355f18
eleccionesgf78 <elecciones.gf78@gmail.com>
2025-11-15 13:30:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ with col1:
     st.image("LOGO_PJ_TERMAS.jpg", width=180)
 
 with col2:
-    st.title("Sorteo extraordinario por una navidad feliz  MINGO 2026")
+    st.title("Sorteo extraordinario por una navidad feliz.        MINGO 2026")
     st.write("Subí un archivo Excel con columnas **dni** y **nombre** para realizar el sorteo.")
 
 st.markdown("---")
